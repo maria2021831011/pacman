9c5adfb5aea78919dda22b59658e686f8e4ac85b
Junayed-SUST <junayedsiddique06@gmail.com>
2024-11-18 23:55:02 -0800

    Update Main.java

--- pacman/src/Pacman/Main.java
+++ pacman/src/Pacman/Main.java
@@ -1,25 +1,22 @@
-package Pacman;
-
-import Pacman.Model;
+package pacman;
 
 import javax.swing.JFrame;
 
-    public class Main extends JFrame{
+public class Pacman extends JFrame{
 
-        public Main() {
-            add(new Model());
-        }
+    public Pacman() {
+        add(new Model());
+    }
 
-
-        public static void main(String[] args) {
-            Main pac = new Main();
-            pac.setVisible(true);
-            pac.setTitle("Pacman");
-            pac.setSize(380,420);
-            pac.setDefaultCloseOperation(EXIT_ON_CLOSE);
-            pac.setLocationRelativeTo(null);
-
-        }
+    //j
+    public static void main(String[] args) {
+        Pacman pac = new Pacman();
+        pac.setVisible(true);
+        pac.setTitle("Pacman");
+        pac.setSize(380,420);
+        pac.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        pac.setLocationRelativeTo(null);
 
     }
 
+}
